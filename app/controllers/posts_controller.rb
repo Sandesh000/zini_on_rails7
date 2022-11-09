0107782b87aaef66785840e40dbfda32ff1c1c6b
Sandesh000 <sandesh.choudhary@taskcraft.in>
2022-11-09 14:51:15 +0530

    third

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,17 @@ class PostsController < ApplicationController
     @posts = Post.all
     render json:@posts
   end
+  def new
+    @post = Post.new
+  end
+  def create
+    @post = Post.create(post_params)
+    
+      render json: @post
+    
+  end 
+  private 
+  def post_params
+    params.require(:post).permit(:name)
+  end
 end
